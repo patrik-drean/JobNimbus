b2144ba64be20c17ff9c66f49b9244328044a8f6
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2023-03-23 14:49:21 +0800

    IsBracketMatch initial approach

---
+++ src/JobNimbus.Tests/BracketsTests.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+
+namespace JobNimbus.Tests;
+
+public class BracketsTests
+{
+    public BracketsTests()
+    {
+        Sut = new Brackets();
+    }
+
+    public Brackets Sut { get; }
+
+    [Theory]
+    [InlineData("{}", true)]
+    [InlineData("{}{}", true)]
+    [InlineData("{{},{}}", true)]
+    [InlineData("{abc...xyz}", true)]
+    [InlineData("", true)]
+    [InlineData("abc...xyz", true)]
+    [InlineData("}{", false)]
+    [InlineData("{{}", false)]
+    [InlineData("{}{{", false)]        
+    public void Should_test_brackets(string input, bool expected)
+    {
+        var result = Sut.IsBracketsMatch(input);
+
+        result.Should().Be(expected);
+    }
+}
---
+++ src/JobNimbus/Brackets.cs
@@ -0,0 +1,31 @@
+﻿namespace JobNimbus;
+
+public class Brackets
+{   
+    public bool IsBracketsMatch(string input)
+    {
+        var openBrackets = new Stack<char>();
+       
+        foreach (var c in input)
+        {
+            if (c == '{')                                  
+                openBrackets.Push(c);
+            
+            else if (c == '}')
+            {                    
+                if (openBrackets.Count == 0)                                         
+                    return false;                    
+
+                var lastOpenBracket = openBrackets.Pop();
+
+                if (lastOpenBracket != '{')                                        
+                    return false;                    
+            }
+        }
+       
+        if (openBrackets.Count > 0)            
+            return false;            
+        
+        return true;
+    }
+}
